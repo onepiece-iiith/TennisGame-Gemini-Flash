0818a7e8e3e4e518635d8e2ca9cd1b8079faac6e
onepieceIsReal <onepiece.iiith@gmail.com>
2025-02-18 14:45:24 +0530

    Refactor: Reduce complexity of TennisGame class

--- TennisGame.java
+++ TennisGame.java
@@ -1,6 +1,4 @@
-
 public class TennisGame1 implements TennisGame {
-    
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -19,58 +17,50 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
+        if (m_score1 == m_score2) {
+            return getEqualScore();
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            return getOverFourScore();
+        } else {
+            return getStandardScore();
+        }
+    }
+
+    private String getEqualScore() {
+        switch (m_score1) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            default:
+                return "Deuce";
+        }
+    }
+
+    private String getOverFourScore() {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) {
+            return "Advantage player1";
+        } else if (minusResult == -1) {
+            return "Advantage player2";
+        } else if (minusResult >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
+        }
+    }
+
+    private String getStandardScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        int[] scores = {m_score1, m_score2};
+        String[] scoreNames = {"Love", "Fifteen", "Thirty", "Forty"};
+
+        for (int i = 0; i < 2; i++) {
+            score += scoreNames[scores[i]] + (i == 0 ? "-" : "");
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
+
         return score;
     }
 }
