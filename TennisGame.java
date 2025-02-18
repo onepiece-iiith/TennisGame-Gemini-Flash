b65003a1db8df1829cae826d0364105b819e7282
onepieceIsReal <onepiece.iiith@gmail.com>
2025-02-18 14:41:45 +0530

    Refactored code to address design smells

--- TennisGame.java
+++ TennisGame.java
@@ -1,6 +1,5 @@
-
 public class TennisGame1 implements TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -19,58 +18,58 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
+        if (m_score1 == m_score2) {
+            return getEqualityScore();
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            return getAdvantageOrWinScore();
+        } else {
+            return getRegularScore();
+        }
+    }
+
+    private String getEqualityScore() {
+        switch (m_score1) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            default:
+                return "Deuce";
+        }
+    }
+
+    private String getAdvantageOrWinScore() {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) return "Advantage player1";
+        else if (minusResult == -1) return "Advantage player2";
+        else if (minusResult >= 2) return "Win for player1";
+        else return "Win for player2";
+    }
+
+    private String getRegularScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        for (int i = 1; i < 3; i++) {
+            int tempScore = (i == 1) ? m_score1 : m_score2;
+            score += (i == 2) ? "-" : "";
+            score += getScoreLookup(tempScore);
         }
         return score;
+    }
+
+    private String getScoreLookup(int tempScore) {
+        switch (tempScore) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return String.valueOf(tempScore);
+        }
     }
 }
